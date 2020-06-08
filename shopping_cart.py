9251bb2490435c0f5af9d91ce75fc56300688a7f
dak529 <dak529@stern.nyu.edu>
2020-06-08 10:47:48 -0400

    Shopping Cart Final

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,13 +1,13 @@
 # shopping_cart.py
 
-# A grocery store name of your choice
-# A grocery store phone number and/or website URL and/or address of choice 
-# The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2020-02-07 03:54 PM)
-# The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $3.50, etc.)
-# The total cost of all shopping cart items (i.e. the "subtotal"), formatted as US dollars and cents (e.g. $19.47), calculated as the sum of their prices
-# The amount of tax owed (e.g. $1.70), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax) - YES
-# The total amount owed, formatted as US dollars and cents (e.g. $21.17), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
-# A friendly message thanking the customer and/or encouraging the customer to shop again
+# A grocery store name of your choice - DONE
+# A grocery store phone number and/or website URL and/or address of choice - DONE
+# The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2020-02-07 03:54 PM) - DONE
+# The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $3.50, etc.) - DONE
+# The total cost of all shopping cart items (i.e. the "subtotal"), formatted as US dollars and cents (e.g. $19.47), calculated as the sum of their prices - DONE
+# The amount of tax owed (e.g. $1.70), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax) - DONE
+# The total amount owed, formatted as US dollars and cents (e.g. $21.17), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items - DONE
+# A friendly message thanking the customer and/or encouraging the customer to shop again - DONE
 
 products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
@@ -54,7 +54,7 @@ subtotal = 0
 groceries = []
 
 while True:
-    item_id = input("Please Enter Item ID Here; Enter DONE when complete: ")
+    item_id = input("Please Enter Item ID; Enter DONE when complete: ")
     
     if item_id == "DONE":
         break
